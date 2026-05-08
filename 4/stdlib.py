c06a4d37dd4ced6136bb7da85310d83b4e6ada89
Caleb Curry <caleb@calebcurry.com>
2026-05-08 10:24:00 -0400

    web scraping

--- 4/stdlib.py
+++ 4/stdlib.py
@@ -3,11 +3,18 @@
 # from pydantic import BaseModel
 # import string
 
-from datetime import datetime, timezone, date, time
+# from datetime import datetime, timezone, date, time
 
-import time as t
+# import time as t
 
-from zoneinfo import ZoneInfo, available_timezones
+# from zoneinfo import ZoneInfo, available_timezones
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+# import json
+# import requests
+import httpx
 
 # # random numbers #
 
@@ -47,63 +54,90 @@ from zoneinfo import ZoneInfo, available_timezones
 
 ## Working with Dates
 
-dt = datetime.now()
+# dt = datetime.now()
 
-print(dt)
+# print(dt)
 
-print(dt.strftime("%Y-%m-%d %H:%M"))
+# print(dt.strftime("%Y-%m-%d %H:%M"))
 
-date_string = "2026-05-01 11:11:00.100000"
+# date_string = "2026-05-01 11:11:00.100000"
 
-dt2 = datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S.%f")
+# dt2 = datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S.%f")
 
-print(dt2)
+# print(dt2)
 
-## Working with just dates ##
+# ## Working with just dates ##
 
-print(date.today())
+# print(date.today())
 
-## Working with just times ##
+# ## Working with just times ##
 
-print(datetime.now().time())
+# print(datetime.now().time())
 
-## timestamps ##
+# ## timestamps ##
 
-print(t.time())
+# print(t.time())
 
-## timezones ##
+# ## timezones ##
 
-dt_zoned = datetime.now(ZoneInfo("America/New_York"))
-print(dt_zoned)
+# dt_zoned = datetime.now(ZoneInfo("America/New_York"))
+# print(dt_zoned)
 
-print(dt_zoned.astimezone(ZoneInfo("America/Los_Angeles")))
+# print(dt_zoned.astimezone(ZoneInfo("America/Los_Angeles")))
 
-print(dt_zoned.astimezone(ZoneInfo("UTC")))
+# print(dt_zoned.astimezone(ZoneInfo("UTC")))
 
-## timing things ##
+# ## timing things ##
 
-# time before
-start = t.time()
+# # time before
+# start = t.time()
 
-# do something
-total = 0
-for i in range(100000000):
-    total += i
-# time after
-end = t.time()
+# # do something
+# total = 0
+# for i in range(100000000):
+#     total += i
+# # time after
+# end = t.time()
 
-# do math
-print(end - start)
+# # do math
+# print(end - start)
 
 
-## perf_counter() ##
+# ## perf_counter() ##
 
-start = t.perf_counter()
+# start = t.perf_counter()
 
-total = 0
-for i in range(100000000):
-    total += i
+# total = 0
+# for i in range(100000000):
+#     total += i
 
-end = t.perf_counter()
+# end = t.perf_counter()
 
-print(end - start)
+# print(end - start)
+
+## Web Requests ##
+
+# url = "https://docs.python.org/3/library/datetime.html#format-codes"
+
+# with urlopen(url) as response:
+#     soup = BeautifulSoup(response.read(), "html.parser")
+
+# print(soup.head.title.text)  # type: ignore
+
+# # get all links
+
+# for link in soup.body.find_all("a"):  # type: ignore
+#     print(link.get("href"))
+
+
+## JSON ##
+
+url = httpx.URL("https://openlibrary.org/search.json?q=python+programming")
+print(url.host, url.path, url.params)
+
+data = httpx.get(url).json()
+
+books = data.get("docs")[:3]
+
+for book in books:
+    print(book.get("title"), end="\n\n")
