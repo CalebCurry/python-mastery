1f8cdaf427eeaa140a2eeed97669fe09925bf8d3
Caleb Curry <caleb@calebcurry.com>
2026-06-19 00:30:17 -0400

    fix text

--- ai/embedder.py
+++ ai/embedder.py
@@ -6,7 +6,7 @@ embedder = OpenAI(base_url=str(settings.embedder_base_url), api_key="example")
 
 
 def embed_text(text: str):
-    response = embedder.embeddings.create(model=settings.embedder_model, input="text")
+    response = embedder.embeddings.create(model=settings.embedder_model, input=text)
     return response.data[0].embedding
 
 
